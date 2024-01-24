1b7b7cb6f67f5ee6e84bfa86afdee8618a1f0f50
Gilad Teller <gilad.teller@toluna.com>
2024-01-24 11:52:25 +0200

    add discount calculation

---
+++ CibusBudget/contentScript.js
@@ -0,0 +1,25 @@
+const discountMultiplier = getDiscount();
+const priceElements = document.getElementsByClassName('card-footer');
+for (let e of priceElements) {
+    addDiscountToElement(e, discountMultiplier);
+}
+
+function getDiscount() {
+    const discountElement = document.getElementsByClassName('mat-tooltip-trigger size-16 white-pink')[0];
+    console.log('discountElement', discountElement);
+    const innerText = discountElement.innerText;
+    console.log('mat-tooltip-trigger size-16 white-pink.innerText', innerText);
+    const discountNumberString = innerText.split(' ')[0].replaceAll(/%/g, '');
+    const discountValue = parseFloat(discountNumberString);
+    const discountMultiplier = (100 - discountValue) / 100;
+    console.log('discountMultiplier', discountMultiplier);
+    return discountMultiplier;
+}
+
+function addDiscountToElement(e, discountMultiplier) {
+    let priceText = e.children[0].innerHTML;
+    let priceNumberString = priceText.replaceAll(/\u20AA/g, "");
+    let priceValue = parseFloat(priceNumberString);
+    let discountPrice = priceValue * discountMultiplier;
+    e.children[0].innerHTML = `<s>${priceText}</s> \u20AA${discountPrice}`;
+}
